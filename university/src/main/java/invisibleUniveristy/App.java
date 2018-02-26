ffc7e0560b1ad36cd424b71c90ce868c9c2cef6a
Jan Miętki <jan.mietki@gmail.com>
2018-02-26 13:48:50 +0100

    Initial commit for the project and initial Travis configuration

---
+++ university/src/main/java/invisibleUniveristy/App.java
@@ -0,0 +1,13 @@
+package invisibleUniveristy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
